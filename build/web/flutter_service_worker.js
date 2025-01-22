6b6c6708d462c815e515b4ba3824d76bb0045a8f
paul@vontos.co <paul@vontos.co>
2025-01-22 16:06:45 +0800

    update application page 2

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4c4eee1cdd0e65ea138f37fca8269a14
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "2bbdcf91bfe5bcf5b530506fd3bba33f",
+"flutter_bootstrap.js": "dacc761dd50b5c0a98c1ddf7e5178bcb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
